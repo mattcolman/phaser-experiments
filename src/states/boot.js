ea362848b60110f8f30673f4f24ca580a23c36ab
Matt Colman <matt.colman@fantasycentre.com>
2016-04-15 17:13:02 +1000

    remove webfontloader

--- src/states/boot.js
+++ src/states/boot.js
@@ -1,5 +1,3 @@
-import WebFont from 'webfontloader';
-import Style from '../style'
 import _ from 'lodash'
 
 class Boot extends Phaser.State {
@@ -13,29 +11,6 @@ class Boot extends Phaser.State {
 
     this.game.setupStage()
     this.game.addStates()
-
-    var fontFamilies = _.values(Style.Font.Panton)
-    console.log('load fontFamilies', fontFamilies)
-
-    // converted from FontSquirrel.com
-    WebFont.load({
-      // google: {
-      //   families: ['Droid Sans', 'Droid Serif']
-      // },
-      custom: {
-        families: fontFamilies,
-        urls: ['./fonts/panton/stylesheet.css']
-      },
-      active: ()=> {
-        // this.game.time.events.add(Phaser.Timer.SECOND, this.fontLoadComplete, this)
-        this.fontLoadComplete()
-      }
-
-    });
-  }
-
-  fontLoadComplete() {
-    console.log('fonts loaded!')
     this.game.startGame()
   }
 
